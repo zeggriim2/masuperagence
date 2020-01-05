442a9e76597088a7a843791c6629ba081c0b3f21
Zeggriim <liloulafoudre@hotmail.fr>
2020-01-05 12:34:42 +0100

    git init

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController {
+
+    /**
+     *@Route("/", name="home")
+     *
+     * @return void
+     */
+    public function index() {
+        return $this->render('pages/home.html.twig');
+    }
+}
