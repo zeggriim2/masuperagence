7fe069e025dff1338a527a69ced4cd62d899be6c
Lilian <liloulafoudre@hotmail.fr>
2020-01-07 12:28:26 +0100

    add new property + add delete property + add flash when add or Del Prope
    - Création de la fonction d'ajout de bien
    - Création de la fonction de suppression de bien
    - Ajout d'alert avec les flash lors de la suppression ou de la modification.

--- src/Controller/admin/AdminPropertyController.php
+++ src/Controller/admin/AdminPropertyController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminPropertyController extends AbstractController {
 
@@ -36,8 +37,32 @@ class AdminPropertyController extends AbstractController {
     }
 
     /**
+     * @Route("/admin/property/create", name="admin.property.new", methods={"GET","POST"})
+     * @return void
+     */
+    public function new(Request $request){
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($property);
+            $this->em->flush();
+            $this->addFlash('success','Votre bien à été créer.');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
      *
-     * @Route("/admin/{id}", name="admin.property.edit")
+     * @Route("/admin/property/{id}", name="admin.property.edit", methods={"GET","POST"})
      */
     public function edit(Property $property, Request $request) {
         $form = $this->createForm(PropertyType::class, $property);
@@ -46,6 +71,7 @@ class AdminPropertyController extends AbstractController {
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->em->flush();
+            $this->addFlash('success','Votre bien à été modifié.');
             return $this->redirectToRoute('admin.property.index');
         }
 
@@ -53,6 +79,24 @@ class AdminPropertyController extends AbstractController {
             'property' => $property,
             'form' => $form->createView()
         ]);
+    }
+
+
+    /**
+     * @Route("/admin/property/{id}", name="admin.property.delete", methods={"DELETE"})
+     * @return void
+     */
+    public function delete(Property $property, Request $request){
+
+        $submittedToken = $request->request->get('token');
+        
+        // 'delete' is the same value used in the template to generate the token
+        if ($this->isCsrfTokenValid('delete', $submittedToken)) {
+            $this->em->remove($property);
+            $this->em->flush();
+        }
+        
+        return $this->redirectToRoute('admin.property.index');
     }
 
 }
